2ca3824cf5c8c70eaf69401d3559543ada54750e
Abdul Razak Shaikh <89abdul@gmail.com>
2020-06-23 18:22:32 +0530

    Add files via upload

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $recipient = "info@carenest.co";
 $subject = "CareNest - Requesting Quote";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "<img src='https://img.icons8.com/dusk/100/000000/thumb-up.png'/> " . "<h2>Congratulation</h2>" . "<h2>Your Request has been send Successfully</h2>";
+echo "<img src='https://img.icons8.com/dusk/100/000000/thumb-up.png'/>" . "<h2>Congratulations</h2>" . "<h2>Your Request has been send Successfully</h2>";
 ?>
 
 
